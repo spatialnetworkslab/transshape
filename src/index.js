fe95e1cbc028f9d26a6c837822fbe8938e9c773f
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-09 14:09:03 +0200

    chore: change import structure (no mixing export whining of rollup)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// no mixed default/named exports
-// export { default } from './transshape.js'
+// Main function
+export { default as transshape } from './transshape.js'
 
+// Potentially convenient tools
 export { default as cutPolygon } from './cutPolygon/cutPolygon.js'
-
 export { default as insertPointsLinearRing } from './insertPointsLinearRing.js'
