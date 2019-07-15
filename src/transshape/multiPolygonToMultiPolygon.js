ae3b901dd69e6c574a4cc616c44aa1610579621a
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-15 15:17:52 +0200

    feat: MultiPolygon -> MultiPolygon (still needs testing tho)

--- src/transshape/multiPolygonToMultiPolygon.js
+++ src/transshape/multiPolygonToMultiPolygon.js
@@ -1,3 +1,47 @@
-export default function multiPolygonToMultiPolygon (from, to) {
+import cutPolygons from '../cutPolygon/cutPolygons.js'
+import matchLinearRings from '../matchLinearRings.js'
+import polygonToPolygon from './polygonToPolygon.js'
+import { combineIntoMultiPolygon, splitMultiPolygon } from '../utils/convertMultiPolygon.js'
 
+export default function multiPolygonToMultiPolygon (from, to) {
+  let fromPolygons = splitMultiPolygon(from)
+  let toPolygons = splitMultiPolygon(to)
+
+  const lengthDifference = fromPolygons.length - toPolygons.length
+
+  if (lengthDifference > 0) {
+    toPolygons = cutPolygons(toPolygons, lengthDifference)
+  }
+
+  if (lengthDifference < 0) {
+    fromPolygons = cutPolygons(fromPolygons, -lengthDifference)
+  }
+
+  const fromOuterRings = fromPolygons.map(polygon => polygon.coordinates[0])
+  const toOuterRings = toPolygons.map(polygon => polygon.coordinates[0])
+
+  const fromOrder = matchLinearRings(fromOuterRings, toOuterRings)
+  fromPolygons = fromOrder.map(i => fromPolygons[i])
+
+  return createInterpolator(from, to, fromPolygons, toPolygons)
+}
+
+function createInterpolator (from, to, fromPolygons, toPolygons) {
+  const polygonInterpolators = []
+
+  for (let i = 0; i < fromPolygons.length; i++) {
+    const fromPolygon = fromPolygons[i]
+    const toPolygon = toPolygons[i]
+
+    polygonInterpolators.push(polygonToPolygon(fromPolygon, toPolygon))
+  }
+
+  return function interpolator (t) {
+    if (t === 0) return from
+    if (t === 1) return to
+
+    return combineIntoMultiPolygon(
+      polygonInterpolators.map(polygonInterpolator => polygonInterpolator(t))
+    )
+  }
 }
