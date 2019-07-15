573e0ded7c8f985d418660f7016a480252356981
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-15 17:49:08 +0200

    feat: added some utils to resize arbitrary geometries

--- sandbox/src/examples/Provinces.svelte
+++ sandbox/src/examples/Provinces.svelte
@@ -1,12 +1,17 @@
 <script>
   import provinces from '../files/provinces.geojson'
   import PolygonToPolygonTransitioner from '../components/PolygonToPolygonTransitioner.svelte'
+  import getBBox from '../utils/getBBox.js'
+  import createGeoTransform from '../utils/createGeoTransform.js'
 
   const geometries = {}
 
   for (let i = 0; i < provinces.features; i++) {
     let geometry = provinces.features[i].geometry
-    geometries[i] = provinces.features[i].geometry
+    let bbox = getBBox(geometry)
+    let transformToFitExample = createGeoTransform(bbox, { x: [0, 800], y: [0, 800] })
+
+    geometries[i] = transformGeometry(geometry, transformToFitExample)
   }
 
 
