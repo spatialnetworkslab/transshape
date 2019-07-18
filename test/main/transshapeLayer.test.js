c93369a0fa793a54396288769b300921c9feed52
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-07-18 11:11:03 +0200

    test: tests passing for transshapeLayer

--- test/main/transshapeLayer.test.js
+++ test/main/transshapeLayer.test.js
@@ -1,4 +1,5 @@
 import { transshapeLayer } from '../../src'
+import roundGeometry from '../roundGeometry.js'
 
 describe('transshapeLayer', () => {
   test('keys overlap', () => {
@@ -70,7 +71,12 @@ describe('transshapeLayer', () => {
           [[2, 2], [6, 2], [6, 6], [3, 6], [2, 5], [2, 2]]
         ]
       },
-      2: {}
+      2: {
+        type: 'Polygon',
+        coordinates: [
+          [[1, 1], [5, 1], [5, 5], [1, 5], [1, 1]]
+        ]
+      }
     }
 
     const toLayer = {
@@ -85,7 +91,17 @@ describe('transshapeLayer', () => {
           ]
         ]
       },
-      3: {}
+      3: {
+        type: 'MultiPolygon',
+        coordinates: [
+          [
+            [[0, 0], [2, 0], [2, 2], [0, 2], [0, 0]]
+          ],
+          [
+            [[4, 4], [6, 4], [6, 6], [4, 6], [4, 4]]
+          ]
+        ]
+      }
     }
 
     const expectedHalfwayLayer = {
@@ -100,12 +116,38 @@ describe('transshapeLayer', () => {
           ]
         ]
       },
-      2: {},
-      3: {}
+      2: {
+        type: 'Polygon',
+        coordinates: [
+          [[2, 2], [4, 2], [4, 4], [2, 4], [2, 2]]
+        ]
+      },
+      3: {
+        type: 'MultiPolygon',
+        coordinates: [
+          [
+            [[2.5, 2.5], [1.5, 2.5], [1.5, 1.5], [2.5, 1.5], [2.5, 2.5]]
+          ],
+          [
+            [[3.5, 3.5], [4.5, 3.5], [4.5, 4.5], [3.5, 4.5], [3.5, 3.5]]
+          ]
+        ]
+      }
     }
 
     const layerInterpolator = transshapeLayer(fromLayer, toLayer)
+    const halfwayLayer = roundLayer(layerInterpolator(0.5))
 
-    expect(layerInterpolator(0.5)).toEqual(expectedHalfwayLayer)
+    expect(halfwayLayer).toEqual(expectedHalfwayLayer)
   })
 })
+
+function roundLayer (layer) {
+  const newLayer = {}
+
+  for (const key in layer) {
+    newLayer[key] = roundGeometry(layer[key])
+  }
+
+  return newLayer
+}
