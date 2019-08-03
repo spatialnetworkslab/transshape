173130545233f9620d4879b83360c507fa50e75c
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2019-08-03 18:43:39 +0200

    test: all tests passing!

--- test/main/multiLineStringToMultiLineString.test.js
+++ test/main/multiLineStringToMultiLineString.test.js
@@ -31,7 +31,35 @@ describe('transshape: MultiLineString to MultiLineString', () => {
     expect(interpolator(0.5)).toEqual(expectedHalfWayMultiLineString)
   })
 
-  // test('different number of sub-LineStrings', () => {
+  test('different number of sub-LineStrings', () => {
+    const fromMultiLineString = {
+      type: 'MultiLineString',
+      coordinates: [
+        [[1, 2], [2, 2]],
+        [[3, 2], [4, 2], [4, 1.5]],
+        [[4, 1.5], [4, 1], [5, 1]]
+      ]
+    }
 
-  // })
+    const toMultiLineString = {
+      type: 'MultiLineString',
+      coordinates: [
+        [[1, 3.5], [2, 4]],
+        [[3, 3], [4, 3], [4, 4], [5, 4]]
+      ]
+    }
+
+    const interpolator = transshape(fromMultiLineString, toMultiLineString)
+
+    const expectedHalfWayMultiLineString = {
+      type: 'MultiLineString',
+      coordinates: [
+        [[1, 2.75], [2, 3]],
+        [[3, 2.5], [4, 2.5], [4, 2.5]],
+        [[4, 2.5], [4, 2.5], [5, 2.5]]
+      ]
+    }
+
+    expect(interpolator(0.5)).toEqual(expectedHalfWayMultiLineString)
+  })
 })
